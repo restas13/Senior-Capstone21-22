dbb1c4ff2c70c9fd0605962ed448b1f3d57e024e
Logan Fron <loganfron@gmail.com>
2021-09-13 08:20:50 -0400

    Start work on player movement script

--- SeniorCapstone2122/Assets/Scripts/PlayerMovement.cs
+++ SeniorCapstone2122/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController playerCC;
+    public float movementSpeed;
+
+    private Vector3 movementVector;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCC = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementVector = transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+        movementVector += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if (playerCC.isGrounded)
+        {
+            playerCC.Move(movementVector);
+        }
     }
 }
