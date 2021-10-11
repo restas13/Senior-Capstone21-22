1194a8b730187450c61f8d38b54f4b27f01a9841
Logan Fron <loganfron@gmail.com>
2021-10-11 08:25:31 -0400

    Mostly fix camera rotation

--- SeniorCapstone2122/Assets/Scripts/PlayerMovement.cs
+++ SeniorCapstone2122/Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed;
     public float sprintMult;
     public Camera mainCam;
-    public Vector3 test;
+    public Vector3 camRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,22 +37,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        test = mainCam.transform.rotation.eulerAngles;
         inputVector = controls.Gameplay.Movement.ReadValue<Vector2>();
-        //movementVector.Set(inputVector.x, 0, inputVector.y);
         movementVector = (transform.forward * inputVector.y) + (transform.right * inputVector.x);
         mouseInput.Set(Mouse.current.delta.x.ReadValue(), Mouse.current.delta.y.ReadValue() * -1);
         transform.Rotate(new Vector3(0, mouseInput.x, 0) * lookSensitivity * Time.deltaTime, Space.Self);
-        //if(mainCam.transform.localRotation.x < 90 && mainCam.transform.localRotation.x > -90)
-            mainCam.transform.Rotate(new Vector3(mouseInput.y, 0, 0) * lookSensitivity * Time.deltaTime, Space.Self);
-        /*else if(mainCam.transform.rotation.x >= 90)
-            mainCam.transform.localRotation = Quaternion.Euler(89f, 0, 0);
-        else if(mainCam.transform.rotation.x <= -90)
-            mainCam.transform.localRotation = Quaternion.Euler(-89f, 0, 0);*/
         if(controls.Gameplay.Pause.triggered)
         {
             Cursor.lockState = CursorLockMode.None;
         }
+        camRotation = new Vector3(camRotation.x, mainCam.transform.rotation.eulerAngles.y, mainCam.transform.rotation.eulerAngles.z);
+        //mainCam.transform.Rotate(new Vector3(mouseInput.y, 0, 0) * lookSensitivity * Time.deltaTime, Space.Self);
+        camRotation += (new Vector3(mouseInput.y, 0, 0) * lookSensitivity * Time.deltaTime);
+        if (camRotation.x < 90 && camRotation.x > -90)
+            mainCam.transform.rotation = Quaternion.Euler(camRotation);
+        else if(camRotation.x >= 90)
+            camRotation.Set(89f, 0, 0);
+        else if(camRotation.x <= -90)
+            camRotation.Set(-89f, 0, 0);
     }
 
     void FixedUpdate()
