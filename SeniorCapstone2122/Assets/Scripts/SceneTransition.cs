6befa9e6a99f9d119a47c65ec770e162eaad47fe
Logan Fron <loganfron@gmail.com>
2022-01-18 08:17:45 -0500

    Scene transition

--- SeniorCapstone2122/Assets/Scripts/SceneTransition.cs
+++ SeniorCapstone2122/Assets/Scripts/SceneTransition.cs
@@ -5,10 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    AsyncOperation loadScene;
     Scene currentScene;
     // Start is called before the first frame update
     void Start()
     {
         currentScene = SceneManager.GetActiveScene();
+    }
+
+    public void HitTrigger()
+    {
+        loadScene = SceneManager.LoadSceneAsync(currentScene.buildIndex + 1, LoadSceneMode.Single);
+        loadScene.allowSceneActivation = false;
+    }
+
+    public void LoadNext()
+    {
+        loadScene.allowSceneActivation = true;
     }
 }
